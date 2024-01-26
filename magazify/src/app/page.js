b924159cdb26d51f155cfc6337b51c466d948e71
thehthakur <thehthakur@gmail.com>
2024-01-26 20:20:09 +0530

    Added basic drop down menu with search option

--- magazify/src/app/page.js
+++ magazify/src/app/page.js
@@ -1,8 +1,11 @@
+import BasicButtonExample from "@/components/BasicButtonExample";
+import Filters from "@/components/Filters";
 import Magazine_Entry from "@/components/Magazine_Entry";
 
 export default function Home() {
   return (
     <>
+      <Filters />
       <Magazine_Entry />
     </>
   );
