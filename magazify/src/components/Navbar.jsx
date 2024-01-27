b66a2f85a8eb4b5ee572615f55a6f3f6dcacb05c
thehthakur <thehthakur@gmail.com>
2024-01-27 16:04:27 +0530

    Updated navbar

--- magazify/src/components/Navbar.jsx
+++ magazify/src/components/Navbar.jsx
@@ -1,63 +1,43 @@
 "use client";
 import Link from "next/link";
-import { useState } from "react";
+import { usePathname } from "next/navigation";
+
 
 function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggle = () => {
-    setIsOpen(!isOpen);
-  };
-
+  const currentRoute = usePathname();
   return (
     <nav className="relative py-4 px-6 flex justify-between items-center bg-white">
       <Link href="/" className="text-3xl font-bold leading-none">
-        <img src="/favicon.png" alt="logo" className="absolute top-2 bottom-0 h-10" />
+        Magazify
       </Link>
-
-      <div className="lg:hidden">
-        <button
-          className="navbar-burger flex items-center text-blue-600 p-3"
-          onClick={toggle}
-        >
-          <svg
-            className="block h-4 w-4 fill-current"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <title>Mobile menu</title>
-            <path d="..." />
-          </svg>
-        </button>
-      </div>
 
       <ul
         className={`hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto lg:flex lg:items-center lg:w-auto lg:space-x-6`}
         aria-labelledby="navbar-menu"
       >
         <li>
-          <Link href="/" className="text-sm text-gray-400 hover:text-gray-500">
+          <Link href="/" className={`text-sm ${currentRoute === '/' ? 'text-blue-600 font-bold' : 'text-gray-400 hover:text-gray-500'}`}>
             Home
           </Link>
         </li>
-        <li className="text-gray-300">
+        <li class="text-gray-300">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             stroke="currentColor"
-            className="w-4 h-4 current-fill"
+            class="w-4 h-4 current-fill"
             viewBox="0 0 24 24"
           >
             <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="..."
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
             />
           </svg>
         </li>
         <li>
-          <Link href="/about" className="text-sm text-blue-600 font-bold">
+          <Link href="/about" className={`text-sm ${currentRoute === '/about' ? 'text-blue-600 font-bold' : 'text-gray-400 hover:text-gray-500'}`}>
             About Us
           </Link>
         </li>
