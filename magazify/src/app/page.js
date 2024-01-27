2c0a13cdfcb8dd2245bde31594fb8592f1a0c5fd
thehthakur <thehthakur@gmail.com>
2024-01-27 13:23:29 +0530

    Navbar and linking to json server done

--- magazify/src/app/page.js
+++ magazify/src/app/page.js
@@ -1,11 +1,16 @@
-import BasicButtonExample from "@/components/BasicButtonExample";
 import Filters from "@/components/Filters";
 import Magazine_Entry from "@/components/Magazine_Entry";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <Filters />
+      <div className="flex justify-center my-8">
+        <Link href="/magazines">
+          <button className="btn-primary">View Tickets</button>
+        </Link>
+      </div>
       <Magazine_Entry />
     </>
   );
