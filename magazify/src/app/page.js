b976fa5c4215fefa36491e9b8749b95c198cbd24
divyawasthi <divy.awasthi@gmail.com>
2024-01-27 14:16:34 +0530

    temp commit

--- magazify/src/app/page.js
+++ magazify/src/app/page.js
@@ -1,8 +1,8 @@
 import Magazine_Entry from "@/components/Magazine_Entry";
-
+import { Route } from "next";
 export default function Home() {
   return (
-    <>
+    <>  
       <Magazine_Entry />
     </>
   );
