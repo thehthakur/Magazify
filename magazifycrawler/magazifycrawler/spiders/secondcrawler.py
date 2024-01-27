544f778a38b0ef8282ebf480707507106bcd0f61
FeynmanLogic <112418441+FeynmanLogic@users.noreply.github.com>
2024-01-27 14:28:19 +0530

    Added a second crawler, though with an error

--- magazifycrawler/magazifycrawler/spiders/secondcrawler.py
+++ magazifycrawler/magazifycrawler/spiders/secondcrawler.py
@@ -3,9 +3,9 @@ from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
 class CrawlingSpider(CrawlSpider):
-    name = "mycrawler"
+    name = "secondcrawler"
     allowed_domains = ["gulmohurquarterly.com", "thebombayreview.com",'https://www.pw.org']
-    start_urls = ["https://www.gulmohurquarterly.com/submission-guidelines"]
+    start_urls = ["https://www.pw.org/literary_magazines?field_genres_value=All&taxonomy_vocabulary_20_tid=All&reading_period_status=All&items_per_page=All&fee=All&unsolicited=All&payment=All"]
 
     rules = (
          Rule(LinkExtractor(allow=('/submission-guidelines','/publication-policy-copyright/','www.pw.org/')), callback='parse_item', follow=True),
@@ -14,20 +14,15 @@ class CrawlingSpider(CrawlSpider):
 
     def parse_item(self, response):
         # Use XPath to target the paragraph with submission deadlines
-        submission_deadline_paragraph = response.xpath('//*[@id="section-5fbb824b53875"]/div[2]/div/div/div/strong[5]/text()').get()
-        submission_deadline_word=response.xpath('//*[@id="content"]/div/div[4]/div[3]/div/ul/li[1]/div[4]').get()
-        if submission_deadline_paragraph:
-            # Yield a dictionary with the extracted information
-            yield {
-                "submission_deadline": submission_deadline_paragraph.strip(),
-                "url": response.url,
-            }
-        elif submission_deadline_word:
+       
+        submission_deadline_word=response.xpath('/html/body/div[1]/div[2]/div/div[1]/div/div[4]/div[3]/div/ul/li/div[4]/span[2]/span').get()
+       
+        if submission_deadline_word:
             yield{
                 "submission_deadline": submission_deadline_word.strip(),
-                "url": response.xpath('//*[@id="content"]/div/div[4]/div[3]/div/ul/li[1]/div[3]/div/a').get()
+                "url": response.xpath('/html/body/div[1]/div[2]/div/div[1]/div/div[4]/div[3]/div/ul/li/div[2]/h2/a').get()
             }
-        else:
+        if not submission_deadline_word:
             print("Submission deadline paragraph not found on this page.")
             print("DEADLINE TEXT NOT FOUND CURRENTLY")
 
